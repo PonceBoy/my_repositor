676a618e95679f9a73aeef1052edaff043cf6e5c
PonceBoy <127787782+PonceBoy@users.noreply.github.com>
2023-04-10 13:20:09 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var url = window.location.href;
-var swLocation  ='sw.js';
+var swLocation  ='/.sw.js';
 
     if(navigator.serviceWorker) {
         if(url.includes('localhost')){
-            swLocation = 'sw.js';
+            swLocation = './sw.js';
         }
         navigator.serviceWorker.register( swLocation);
 }
